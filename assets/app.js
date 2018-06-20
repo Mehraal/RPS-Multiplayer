b67a7f0c2ec67c688dafdc652dfb23e2a34ffb18
Alex Mehr <alirezaalipouri@gmail.com>
2018-06-20 00:59:37 -0700

    style changed

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,7 @@
-var playerOneChoices = ["r", "p", "s"];
-var playerTwoChoices = ["r", "p", "s"];
+var player1 = " ";
+var player2 = " ";
+var player1Choices = ["r", "p", "s"];
+var player2Choices = ["r", "p", "s"];
 
 var wins = 0;
 var losses = 0;
@@ -8,48 +10,50 @@ var ties = 0;
 
 document.onkeyup = function (event) {
 
-    var playerOneChoices = event.key;
+    var player1Guess = event.key;
+    var compare = function(player1Guess, player2Guess){
 
-    // var playerOneGuess = playerOneChoices[Math.floor(Math.random() * playerOneChoices.length)];
+    var player1Guess = player1Choices[Math.floor(Math.random() * player1Choices.length)];
+    var player2Guess = player2Choices[Math.floor(Math.random() * player2Choices.length)];
 
-    if ((playerOneChoices === "r") || (playerOneChoices === "p") || (playerOneChoices === "s")) {
+    if ((player1Guess === "r") || (player1Guess=== "p") || (player1Guess === "s")) {
 
 
-        if ((playerOneChoices === "r") && (playerTwoChoices === "p")) {
-            losses++;
+        if ((player1Guess === "r") && (player2Guess === "p")) {
+          return(console.log("player2Losses"));
         }
 
-        if ((playerOneChoices === "p") && (playerTwoChoices === "s")) {
-            losses++;
+        if ((player1Guess === "p") && (player2Guess === "s")) {
+           return (console.log("player1Losses"));
         }
 
-        if ((playerOneChoices === "r") && (playerTwoChoices === "s")) {
-            wins++;
+        if ((player1Guess === "r") && (player2Guess === "s")) {
+            return(console.log("player1Wins"));
         }
 
-        if ((playerOneChoices === "p") && (playerTwoChoices === "r")) {
-            wins++;
+        if ((player1Guess === "p") && (player2Guess === "r")) {
+            return(console.log("player1Wins"));
         }
 
-        if ((playerOneChoices === "s") && (playerTwoChoices === "r")) {
-            losses++;
+        if ((player1Guess === "s") && (player2Guess === "r")) {
+            return(console.log("playe1Losses"));
         }
 
-        if ((playerOneChoices === "s") && (playerTwoChoices === "p")) {
-            wins++;
+        if ((player1Guess === "s") && (player2Guess === "p")) {
+            return(console.log("player1Wins"));
         }
 
-        if (playerOneChoices === playerTwoChoices) {
-            ties++;
-        }
-        var html =
-            "<p>You chose: " + playerOneChoices + "</p>" +
-            "<p>The playerTwo chose: " + playerTwoChoices + "</p>" +
-            "<p>wins: " + wins + "</p>" +
-            "<p>losses: " + losses + "</p>" +
-            "<p>ties: " + ties + "</p>";
-
-        document.querySelector("#game").innerHTML = html;
-
+        if (player1Guess === player2Guess) {
+           return(console.log("you tied, try again"));
+        } 
     }
-};
+        // var html =
+        //     "<p>You chose: " + player1Guess + "</p>" +
+        //     "<p>The playerTwo chose: " + player2Guess + "</p>" +
+        //     "<p>wins: " + wins + "</p>" +
+        //     "<p>losses: " + losses + "</p>" +
+        //     "<p>ties: " + ties + "</p>";
+
+        // document.querySelector("#game").innerHTML = html;
+    }
+}
